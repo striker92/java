4c22998a558d777d9579b3885c86639e346b3792
Serhii Khizhniak <strikerko.08@mail.ru>
2017-02-06 12:21:40 +0200

    Add files via upload

---
+++ Calendar.java
@@ -0,0 +1,57 @@
+package february;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.time.*;
+
+public class Calendar {
+	@SuppressWarnings("deprecation")
+	public static int[] Cal(int month, int year) throws IOException {
+		LocalDate localDate = LocalDate.of(year, month, 1);
+		DayOfWeek weekday = localDate.getDayOfWeek();
+		int value = weekday.getValue();
+	//	System.out.println();
+	//	System.out.println(localDate.getDayOfWeek());
+	//	System.out.println(localDate.plusDays(1));
+		int[] days = {0, 31 ,28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };	
+		int[] mas = new int[42];
+		int count = 0;
+		for(int i= value-1; i<days[month]+value-1;i++)
+		{	
+			count++;
+			mas[i]  = count;
+		}	
+		int N = 0;
+		System.out.print("  MON TUE  WED  THU  FRI  SUT  SUN\n");
+		for (int i = 0; i < mas.length; i++, System.out.print("  ")) 
+		{
+		
+			 if (mas[i]  == 0)
+				 {
+					 System.out.print("    ");
+				 }
+			 else
+				 {
+					 if (mas[i] <10 )
+					 {
+						 System.out.print(" ");
+						 
+					 }
+					 System.out.print(mas[i] + " ");
+				 }		
+				 N++;
+				 if (N==7)
+				 {
+					 System.out.print ("\n");
+					 N=0;
+				 }
+		}		 
+					 
+	return mas;
+	}
+
+	public static void main(String[] args) throws IOException
+	{
+		Cal(3, 2017);
+	}
+}
